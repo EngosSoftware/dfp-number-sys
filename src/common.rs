08b7fc5f5d2f825b1fd5fc0655af4c19902bf9a0
wisbery <146648557+wisbery@users.noreply.github.com>
2023-12-22 17:30:09 +0100

    Updates.

--- src/common.rs
+++ src/common.rs
@@ -1,6 +1,6 @@
 //! # Common definitions
 
-/// 128-bit decimal.
+/// A structure representing a 128-bit decimal.
 #[repr(C, align(16))]
 #[derive(Debug, Copy, Clone)]
 pub struct BID128 {
@@ -9,14 +9,19 @@ pub struct BID128 {
 
 /// Exception flag `Invalid` as [u32] value.
 pub const FB_INVALID: u32 = FlagBits::Invalid as u32;
+
 /// Exception flag `ZeroDivide` as [u32] value.
 pub const FB_ZERO_DIVIDE: u32 = FlagBits::ZeroDivide as u32;
+
 /// Exception flag `Overflow` as [u32] value.
 pub const FB_OVERFLOW: u32 = FlagBits::Overflow as u32;
+
 /// Exception flag `Underflow` as [u32] value.
 pub const FB_UNDERFLOW: u32 = FlagBits::Underflow as u32;
+
 /// Exception flag `Inexact` as [u32] value.
 pub const FB_INEXACT: u32 = FlagBits::Inexact as u32;
+
 /// Exception flag `AllClear` as [u32] value.
 pub const FB_CLEAR: u32 = FlagBits::AllFlagsClear as u32;
 
@@ -33,12 +38,16 @@ pub enum FlagBits {
 
 /// Rounding mode `NearestEven` as [u32] value.
 pub const RM_NEAREST_EVEN: u32 = RoundingModes::NearestEven as u32;
+
 /// Rounding mode `Downward` as [u32] value.
 pub const RM_DOWNWARD: u32 = RoundingModes::Downward as u32;
+
 /// Rounding mode `Upward` as [u32] value.
 pub const RM_UPWARD: u32 = RoundingModes::Upward as u32;
+
 /// Rounding mode `TowardZero` as [u32] value.
 pub const RM_TOWARD_ZERO: u32 = RoundingModes::TowardZero as u32;
+
 /// Rounding mode `NearestAway` as [u32] value.
 pub const RM_NEAREST_AWAY: u32 = RoundingModes::NearestAway as u32;
 
